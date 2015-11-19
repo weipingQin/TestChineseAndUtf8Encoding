7b20d4b1590e351dc7f54eb684c940a5f354c2f1
weipingQin <995308963@qq.com>
2015-11-19 11:26:57 +0800

    解析今天在知乎上看到的一个朋友的解析出来的utf-8编码

--- src/com/test/qinweiping/TestUtf8Encoding.java
+++ src/com/test/qinweiping/TestUtf8Encoding.java
@@ -1,20 +1,36 @@
 package com.test.qinweiping;
 
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.SynchronousQueue;
 
 public class TestUtf8Encoding {
 	
 	public static void main(String[] args) {
 		String testUtf8String = "\u4e2d\u56fd\u4f01\u4e1a\u5bb6\u6742\u5fd7";
+		
+
+		String testString1 = "\u9a8c\u8bc1\u7801\u9519\u8bef0";
+		
+		String testString2 = "\u9a8c\u8bc1\u7801\u9519\u8bef";
+		
 		try{
 			TestUtf8Encoding test = new TestUtf8Encoding();
-			String chineseStr = test.decodingUtf8(testUtf8String);
-			System.out.println("将utf-8编码转化成中文:"+chineseStr);
+			String chineseStr = test.decodingUtf8(testString1);
+			String testChineseStr = test.decodingUtf8(testString2);
+			System.out.println("知乎网友模拟登录将utf-8编码转化成中文:"+chineseStr);
+			System.out.println("知乎网友模拟登录解出来的字符:"+testChineseStr);
 		}catch(UnsupportedEncodingException e){
 			e.printStackTrace();
 		}
 		String result = TestUtf8Encoding.encodingGBK("中国企业家杂志");
 		System.out.println("将中文转化成utf-8编码:"+result);
+		
+		String testString = TestUtf8Encoding.stringConvertHexCharacter("中国企业家杂志");
+		System.out.println(testString);
+		
+		
+		//String utf8String = TestUtf8Encoding.formatUft8String(testString);
+	//	System.out.println("formatutf-8String"+utf8String);
 		
 	}
 	
@@ -40,6 +56,35 @@ public class TestUtf8Encoding {
 			sb.append("\\u" +Integer.toString(gbkString.charAt(i), 16));
 		}
 		return sb.toString();
+	}
+	
+	/**
+	 * 将中文字符转化成十六进制的字符数组
+	 * @return gbkString
+	 */
+	private static String stringConvertHexCharacter(String gbkString){
+		String result = "";
+		for(int i = 0 ; i < gbkString.length();i++){
+			int ch = (int)gbkString.charAt(i);
+			String s = Integer.toHexString(ch);
+			result += s;
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * 将16进制数格式化一下 就变成了utf-8编码 
+	 */
+	private static String formatUft8String(String hexString){
+		String result = "";
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < hexString.length() ; i++){
+			result +=hexString.substring(i*4,hexString.length());
+			//break;
+			result += hexString.substring(i*4,i*4+4)+"\\u";
+		}
+		return result;
 	}
 
 
